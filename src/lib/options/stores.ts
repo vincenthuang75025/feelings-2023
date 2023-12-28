7bda7a4e201cf35e1c221cda303c9d7abd609987
Vincent Huang <vincenthuang75025@yahoo.com>
2023-12-28 12:47:14 -0600

    Blank

--- src/lib/options/stores.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte-local-storage-store';
-import { defaultOptions } from './defaultOptions';
-
-export const options = writable('options', defaultOptions);
-
-export const userDefaultOptions = writable('userDefaultOptions', defaultOptions);
--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Spinner from '$lib/components/Spinner.svelte';
-</script>
-
-<svelte:head>
-  <title>Spinner Dinner</title>
-</svelte:head>
-
-<Spinner />
